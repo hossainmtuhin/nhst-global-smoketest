b91c32f6d88371eae1ac851e926a2039af932306
hossainmtuhin <hossain.m.tuhin@nhst.com>
2020-02-26 18:54:35 +0600

    test setup to remoteWebdriver

---
+++ src/test/java/com/nhst/smoketest/Tests/SetupTest.java
@@ -0,0 +1,37 @@
+package com.nhst.smoketest.Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+
+public class SetupTest {
+
+    private WebDriver driver = null;
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        driver = new RemoteWebDriver(new URL("http://nhst-test-automation.test.nhst.cloud/wd/hub"), DesiredCapabilities.chrome());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void loginTest() {
+        driver.get("https://www.tradewindsnews.com/");
+        String metaTitle = driver.getTitle();
+        System.out.println("homepage title is: " +metaTitle);
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
